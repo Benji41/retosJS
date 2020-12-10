4c8bdb15b73f5c56d8b865750046e48eacf02ebe
Benji41 <noereynoso.business@gmail.com>
2020-12-09 19:11:25 -0700

    Tercer reto fd2

---
+++ 02-Fundamentals-Part-2/reto3.js
@@ -0,0 +1,25 @@
+'use strict';
+const MarkMiller = {
+  fullname: 'Mark Miller',
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    //aqui se crea un atributo dentro de la funcion
+    return (this.bmi = this.mass / this.height ** 2);
+  },
+};
+const JohnSmith = {
+  fullname: 'John Smith',
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    return (this.bmi = this.mass / this.height ** 2);
+  },
+};
+MarkMiller.calcBMI();
+JohnSmith.calcBMI();
+console.log(
+  MarkMiller.bmi > JohnSmith.bmi
+    ? `Mark's BMI is higher than John's (${MarkMiller.bmi} vs ${JohnSmith.bmi})`
+    : `John's BMI is higher than Mark's (${JohnSmith.bmi} vs ${MarkMiller.bmi})`
+);
